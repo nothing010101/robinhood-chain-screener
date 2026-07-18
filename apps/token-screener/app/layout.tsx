9420b857bc9ef02ff81d723b0e4530f76b7f45ac
nothing010101 <mstrj719@gmail.com>
2026-07-18 11:11:35 +0000

    add virtual protocol site verification meta tag to app subdomain

--- apps/token-screener/app/layout.tsx
+++ apps/token-screener/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <meta name="virtual-protocol-site-verification" content="c9db491412069e78c2597677b645bdb1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
